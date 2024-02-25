562d363563f93ac6d92a3fade68e7d1efd8e51d8
Deepanshu Dheer <103347008+deepanshudheer@users.noreply.github.com>
2024-02-25 11:24:02 +0530

    Update first.js

--- react-js/first.js
+++ react-js/first.js
@@ -1,1 +1,18 @@
+function Person(props) {
+  return (
+    <div class="person">
+      <h1>{props.name}</h1>
+      <p>Age is {props.age}</p>
+    </div>
+  );
+}
 
+var app = (
+  <div>
+    <Person name="Deepsi" age="20" />
+    <Person name="Vicky" age="33" />
+  </div>
+);
+ReactDOM.render(app, document.querySelector('#app'));
+
+// reusable components to display div
