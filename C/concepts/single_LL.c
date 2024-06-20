1e818bf6fa3b6adba9b77b125e13712be285e56d
Deepanshu Dheer <103347008+deepanshudheer@users.noreply.github.com>
2024-06-20 14:14:36 +0530

    Create single_LL.c

---
+++ C/concepts/single_LL.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+        struct Node* temp = *head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+void display(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+void reverse(struct Node** head) {
+    struct Node* prev = NULL;
+    struct Node* current = *head;
+    struct Node* next = NULL;
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    *head = prev;
+}
+
+int main() {
+    struct Node* head = NULL;
+    
+    insertEnd(&head, 2);
+    insertEnd(&head, 1);
+    insertEnd(&head, 0);
+    insertEnd(&head, 0);
+    insertEnd(&head, 1);
+    insertEnd(&head, 1);
+    insertEnd(&head, 2);
+    insertEnd(&head, 2);
+
+    printf("Original linked list: ");
+    display(head);
+
+    reverse(&head);
+
+    printf("Reversed linked list: ");
+    display(head);
+
+    return 0;
+}
