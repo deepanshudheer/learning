481ec590f5f40a844a85045193b7a1d3903d7e2e
Deepanshu Dheer <103347008+deepanshudheer@users.noreply.github.com>
2024-07-30 09:34:31 +0530

    Create area_rectangle.cpp

---
+++ codeninjas/area_rectangle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Write your code here
+    int length, breadth;
+    cin >> length >> breadth;
+    cout << length * breadth;
+    return 0;
+}
