8e7b147c4c9e31cbf87aae5c23132178daca817d
Deepanshu Dheer <103347008+deepanshudheer@users.noreply.github.com>
2024-08-08 10:28:10 +0530

    Create 169majority_element.cpp

---
+++ leetcode/169majority_element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int n = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                n = num;
+            }
+            count += (num == n) ? 1 : -1;
+        }
+        return n;
+    }
+};
